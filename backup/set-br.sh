d79c8001d2ce86a957ff0bc9a19d6742aa24ab3a
shamrin-18 <102512110+shamrin-18@users.noreply.github.com>
2022-07-23 16:58:48 +0800

    Add files via upload

---
+++ backup/set-br.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# My Telegram : https://t.me/shamrin-18
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/shamrin-18/perizinan/main/ipvps.txt | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/shamrin-18"
+exit 0
+fi
+# Link Hosting Kalian
+shamrin-18="raw.githubusercontent.com/shamrin-18/scriptvps/main/backup"
+
+curl https://rclone.org/install.sh | bash
+printf "q\n" | rclone config
+wget -O /root/.config/rclone/rclone.conf "https://${shamrin-18}/rclone.conf"
+git clone  https://github.com/magnific0/wondershaper.git
+cd wondershaper
+make install
+cd
+rm -rf wondershaper
+echo > /home/limit
+apt install msmtp-mta ca-certificates bsd-mailx -y
+cat<<EOF>>/etc/msmtprc
+defaults
+tls on
+tls_starttls on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+
+account default
+host smtp.gmail.com
+port 587
+auth on
+user shamrin-18store@gmail.com
+from shamrin-18store@gmail.com
+password xbnelgkimafglnva 
+logfile ~/.msmtp.log
+EOF
+chown -R www-data:www-data /etc/msmtprc
+cd /usr/bin
+wget -O autobackup "https://${shamrin-18}/autobackup.sh"
+wget -O backup "https://${shamrin-18}/backup.sh"
+wget -O restore "https://${shamrin-18}/restore.sh"
+wget -O strt "https://${shamrin-18}/strt.sh"
+wget -O limitspeed "https://${shamrin-18}/limitspeed.sh"
+chmod +x autobackup
+chmod +x backup
+chmod +x restore
+chmod +x strt
+chmod +x limitspeed
+cd
+rm -f /root/set-br.sh
