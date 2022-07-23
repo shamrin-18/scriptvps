48a928ad94cd0e40545df1e1c76adcc187dfd418
shamrin-18 <102512110+shamrin-18@users.noreply.github.com>
2022-07-23 18:02:07 +0800

    Add files via upload

--- trojango/deltrgo.sh
+++ trojango/deltrgo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/colongvpn
+# My Telegram : https://t.me/shamrin-18
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -14,13 +14,13 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/colongvpn/perizinan/main/ipvps.txt | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/shamrin-18/perizinan/main/ipvps.txt | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
 echo -e "${NC}${RED}Permission Denied!${NC}";
 echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/colongvpn"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/shamrin-18"
 exit 0
 fi
 clear
@@ -59,4 +59,4 @@ echo "============================"
 echo "Username : $user"
 echo "Expired  : $exp"
 echo "============================"
-echo "Script By colongvpn"
+echo "Script By shamrin-18"
